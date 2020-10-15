6f41a6a8b8c211afb2e83052d542396cccbe22a6
qpcon <silenyate@gmail.com>
2020-10-15 18:11:26 +0800

    碰撞傷害完成 爆炸特效 遊戲結束美術完成

--- Galaxian.js
+++ Galaxian.js
@@ -4,7 +4,7 @@ window.onload=function Start(){
     Main.style.top=215;
     Main.style.left=295;
     
-    setInterval(function Damege(){
+setInterval(function Damege(){
     var MainJS=document.getElementById('Main');
     var MainX=MainJS.getBoundingClientRect().left;
     var MainY=MainJS.getBoundingClientRect().top;
@@ -136,16 +136,16 @@ function Move(No){
     var Teki9Y=Teki9.getBoundingClientRect().top;
 
     var Teki10=document.getElementById('Teki10');
-    var Teki2X=Teki10.getBoundingClientRect().left;
-    var Teki2Y=Teki10.getBoundingClientRect().top;
+    var Teki10X=Teki10.getBoundingClientRect().left;
+    var Teki10Y=Teki10.getBoundingClientRect().top;
 
     var Teki11=document.getElementById('Teki11');
-    var Teki2X=Teki11.getBoundingClientRect().left;
-    var Teki2Y=Teki11.getBoundingClientRect().top;
+    var Teki11X=Teki11.getBoundingClientRect().left;
+    var Teki11Y=Teki11.getBoundingClientRect().top;
 
     var Teki12=document.getElementById('Teki12');
-    var Teki2X=Teki12.getBoundingClientRect().left;
-    var Teki2Y=Teki12.getBoundingClientRect().top;
+    var Teki12X=Teki12.getBoundingClientRect().left;
+    var Teki12Y=Teki12.getBoundingClientRect().top;
 
     var Teki13=document.getElementById('Teki13');
     var Teki13X=Teki13.getBoundingClientRect().left;
@@ -155,22 +155,203 @@ function Move(No){
     var Teki14X=Teki14.getBoundingClientRect().left;
     var Teki14Y=Teki14.getBoundingClientRect().top;
 
-    console.log(MainX);
-    console.log(MainY);
-    console.log(TekiY);
-    console.log(TekiX);
+    var DamegiY=Math.abs(MainY-TekiY);
+    var DamegiX=Math.abs(MainX-TekiX);
+   
+    var Damegi2Y=Math.abs(MainY-Teki2Y);
+    var Damegi2X=Math.abs(MainX-Teki2X);
     
-    var DamegiPointY=TekiY+50;
-    var DamegiPointX=TekiX+50;
+    var Damegi3Y=Math.abs(MainY-Teki3Y);
+    var Damegi3X=Math.abs(MainX-Teki3X);
+    
+    var Damegi4Y=Math.abs(MainY-Teki4Y);
+    var Damegi4X=Math.abs(MainX-Teki4X);
+   
+    var Damegi5Y=Math.abs(MainY-Teki5Y);
+    var Damegi5X=Math.abs(MainX-Teki5X);
+    
+    var Damegi6Y=Math.abs(MainY-Teki6Y);
+    var Damegi6X=Math.abs(MainX-Teki6X);
+    
+    var Damegi7Y=Math.abs(MainY-Teki7Y);
+    var Damegi7X=Math.abs(MainX-Teki7X);
+   
+    var Damegi8Y=Math.abs(MainY-Teki8Y);
+    var Damegi8X=Math.abs(MainX-Teki8X);
+    
+    var Damegi9Y=Math.abs(MainY-Teki9Y);
+    var Damegi9X=Math.abs(MainX-Teki9X);
+    
+    var Damegi10Y=Math.abs(MainY-Teki10Y);
+    var Damegi10X=Math.abs(MainX-Teki10X);
+    
+    var Damegi11Y=Math.abs(MainY-Teki11Y);
+    var Damegi11X=Math.abs(MainX-Teki11X);
+    
+    var Damegi12Y=Math.abs(MainY-Teki12Y);
+    var Damegi12X=Math.abs(MainX-Teki12X);
+    
+    var Damegi13Y=Math.abs(MainY-Teki13Y);
+    var Damegi13X=Math.abs(MainX-Teki13X);
+    
+    var Damegi14Y=Math.abs(MainY-Teki14Y);
+    var Damegi14X=Math.abs(MainX-Teki14X);
 
-    if (TekiY<=MainY){
-        if(DamegiPointY>=MainY){
-            if(TekiX<=MainX){
-                if(DamegiPointX>=MainX){
-                    alert('We have maaaaaaaaap');
+
+    if(DamegiY>=0){
+        if(DamegiY<=45){
+            if(DamegiX>=0){
+                if(DamegiX<=25){
+                    GameOver();
+                }
+            }
+        }
+    }
+    
+    if(Damegi2Y>=0){
+        if(Damegi2Y<=45){
+            if(Damegi2X>=0){
+                if(Damegi2X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }
+    
+    if(Damegi3Y>=0){
+        if(Damegi3Y<=45){
+            if(Damegi3X>=0){
+                if(Damegi3X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }    
+    
+    if(Damegi4Y>=0){
+        if(Damegi4Y<=45){
+            if(Damegi4X>=0){
+                if(Damegi4X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }    
+    
+    if(Damegi5Y>=0){
+        if(Damegi5Y<=45){
+            if(Damegi5X>=0){
+                if(Damegi5X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }    
+    
+    if(Damegi6Y>=0){
+        if(Damegi6Y<=45){
+            if(Damegi6X>=0){
+                if(Damegi6X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }    
+
+    if(Damegi7Y>=0){
+        if(Damegi7Y<=45){
+            if(Damegi7X>=0){
+                if(Damegi7X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }
+    
+    if(Damegi8Y>=0){
+        if(Damegi8Y<=45){
+            if(Damegi8X>=0){
+                if(Damegi8X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }  
+    
+    if(Damegi9Y>=0){
+        if(Damegi9Y<=45){
+            if(Damegi9X>=0){
+                if(Damegi9X<=25){
+                    GameOver();
                 }
             }
         }
     }
 
-};
+    if(Damegi10Y>=0){
+        if(Damegi10Y<=45){
+            if(Damegi10X>=0){
+                if(Damegi10X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }
+
+    if(Damegi11Y>=0){
+        if(Damegi11Y<=45){
+            if(Damegi11X>=0){
+                if(Damegi11X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }
+    
+    if(Damegi12Y>=0){
+        if(Damegi12Y<=45){
+            if(Damegi12X>=0){
+                if(Damegi12X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }
+    
+    if(Damegi13Y>=0){
+        if(Damegi13Y<=45){
+            if(Damegi13X>=0){
+                if(Damegi13X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }
+    
+    if(Damegi14Y>=0){
+        if(Damegi14Y<=45){
+            if(Damegi14X>=0){
+                if(Damegi14X<=25){
+                    GameOver();
+                }
+            }
+        }
+    }    
+/* 傷害判定的式子 先抓出主角和怪物的座標 然後利用加法求正數的方法算出距離加以判定 */
+}
+
+function GameOver(){
+    document.getElementById('Main').style.backgroundImage="url('Pic/Explosion.gif')";
+    var GameOver=0
+    var ExplosionGif=setInterval(function Explosion(){
+        if (GameOver==1){
+            document.body.innerHTML="<img src='Pic/GameOver.png'></img>"
+            ClearInterval(ExplosionGif);
+            
+        }
+        document.getElementById('Main').style.backgroundImage="url('Pic/Non.png')";
+
+        GameOver++;
+                },600)
+
+}
