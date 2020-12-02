7fd556429fe9a5f495fd92144590ff497768b0bf
qpcon773 <70684092+qpcon773@users.noreply.github.com>
2020-12-02 18:19:06 +0800

    Update Galaxian.js

--- Galaxian.js
+++ Galaxian.js
@@ -432,7 +432,7 @@ function GameOver(){
             BGMSwitch(2);
             BGMSwitch(3);
 
-           document.body.innerHTML="<img src='./Pic/GameOver.png' class='Background'</img>\
+           document.body.innerHTML="<img src='Pic/GameOver.png' class='Background'</img>\
            <span class='GameOverButton' onclick='Reset();'></span>"
             
             
